897cc9f5fd1014332350958e3b568f4621e05b01
Adam Chainz <adam@adamj.eu>
2015-05-16 15:28:36 +0100

    Output individual benchmarks

--- benchmark_cache/management/commands/benchmark_caches.py
+++ benchmark_cache/management/commands/benchmark_caches.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals
 
+from collections import OrderedDict
 import time
 from random import randint
 
@@ -12,14 +13,20 @@ from texttable import Texttable
 
 
 class Command(BaseCommand):
+    args = "<cache_alias>"
+
     help = """Benchmarks the caches."""
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *aliases, **kwargs):
         results = {}
         for alias in settings.CACHES:
 
             # The alias 'default' isn't very explanatory, so ignore it
             if alias == 'default':
+                continue
+
+            # Aliases specified on commandline = filter
+            if aliases and alias not in aliases:
                 continue
 
             results.update(self.do_benchmark(alias))
@@ -31,34 +38,39 @@ class Command(BaseCommand):
         self.stdout.write("Benchmarking {}".format(alias))
 
         cache.clear()
-
-        start = time.time()
+        timings = OrderedDict()
 
         # sets
+        start = time.time()
         for i in six.moves.range(1000):
             cache.set(self.random_key(), self.random_binary())
+        timings['set'] = time.time() - start
 
         # set_manys
+        start = time.time()
         for i in six.moves.range(1000):
             cache.set_many({
                 self.random_key(): self.random_binary()
                 for x in six.moves.range(40)
             })
+        timings['set_many'] = time.time() - start
 
         # gets
+        start = time.time()
         for i in six.moves.range(1000):
             cache.get(self.random_key())
+        timings['get'] = time.time() - start
 
         # get_many
+        start = time.time()
         for i in six.moves.range(1000):
             cache.get_many([
                 self.random_key()
                 for x in six.moves.range(40)
             ])
+        timings['get_many'] = time.time() - start
 
-        end = time.time()
-
-        return {alias: (end - start)}
+        return {alias: timings}
 
     def random_key(self):
         return "Key{}".format(randint(1, 500))
@@ -67,11 +79,18 @@ class Command(BaseCommand):
         return randint(1, 1024 ** 1) * six.binary_type(randint(0, 255))
 
     def output_results(self, results):
-        table = Texttable()
-        table.set_cols_align(["l", "r"])
-        table.add_row(["Cache", "Time for benchmark"])
+        table = Texttable(max_width=0)
 
-        for name, benchmark_time in six.iteritems(results):
-            table.add_row([name, '{}'.format(benchmark_time)])
+        header = ["Cache Alias"]
+        for key in next(six.itervalues(results)):
+            header += ["{} benchmark".format(key)]
+        table.add_row(header)
+        table.set_cols_align(["l"] + (["r"] * (len(header) - 1)))
+
+        for alias, timings in six.iteritems(results):
+            row = [alias]
+            for benchmark, total in six.iteritems(timings):
+                row += ['{}'.format(total)]
+            table.add_row(row)
 
         self.stdout.write(table.draw())
